6137b9d6634616209bc42fb69c7261614040fab0
HydraDev25 <bmw.bmv421@gmail.com>
2025-08-29 11:31:38 +0300

    Add files via upload

---
+++ Hydra-Dev-Anti-Rpf/config.lua
@@ -0,0 +1,5 @@
+Config = {}
+
+Config.MaxHasar = 80 -- vurulan hasar 80 ve 97 arasında ise kickler (sadece yumruk)
+Config.WebhookURL = "SENİN_WEBHOOKUN" -- detaylı log webhook
+Config.YumrukHash = 2725352035 -- yumruk
---
+++ Hydra-Dev-Anti-Rpf/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'm4f3a.lua'
+description 'tek yumruk rpf engelleyici'
+
+server_script 'config.lua'
+server_script 'server.lua'
